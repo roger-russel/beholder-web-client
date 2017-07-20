2254974f569f521c070f32ef1febcfd4a12c5c7b
Roger Russel <roger.neris@locaweb.com.br>
2017-07-20 15:39:54 -0300

    include one more status

--- src/Eyes/Url/UrlStatus.php
+++ src/Eyes/Url/UrlStatus.php
@@ -6,16 +6,15 @@ use BeholderWebClient\Eyes\Status;
 
 class UrlStatus extends Status {
 
-  const COULD_NOT_CONNECT = 'Could not connect';
+  const REQUERIMENT_FAIL = 'REQUERIMENT FAIL: php lib curl is not installed.';
+
+  const COULD_NOT_CONNECT = 'Could not connect to url: ';
   const COULD_NOT_CONNECT_NUMBER = 600;
 
-  const COULD_NOT_CONNECT_URL = 'Could not connect to url: ';
-  const COULD_NOT_CONNECT_URL_NUMBER = 601;
+  const STATUS_CODE_WAS_NOT_EXPECTED = 'The status code returned was not the expected, received: ';
+  const STATUS_CODE_WAS_NOT_EXPECTED_NUMBER = 601;
 
-  const STATUS_CODE_WAS_NOT_EXPECTED = 'The status code returned was not the expected';
-  const STATUS_CODE_WAS_NOT_EXPECTED_NUMBER = 602;
-
-  const EXPECTED_AS = 'It expected a status code 200, but return: ';
-  const EXPECTED_AS_NUMBER = 603;
+  const METHOD_NOT_ALLOWED = 'Method is not allowed: ';
+  const METHOD_NOT_ALLOWED_NUMBER = 602;
 
 }
