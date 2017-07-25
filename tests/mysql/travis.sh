66190fce3e2e1dc19f56130cf974777dedb5d74f
Roger Russel <nemesis@rrussel.org>
2017-07-25 20:11:05 -0300

    craete database

--- tests/mysql/travis.sh
+++ tests/mysql/travis.sh
@@ -5,3 +5,5 @@ DIRFILE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo /etc/init.d/mysql stop
 
 docker-compose -f $DIRFILE/docker-compose.yml up -d
+
+docker exec -it beholder-test-mysql bash -c 'mysql -e "create database IF NOT EXISTS beholder_test;" -uroot -pinitial1234;'
