73a62cc6d03f29818bebfa3078548bf2b7d78ee2
Roger Russel <roger.neris@locaweb.com.br>
2017-07-28 12:02:06 -0300

    finished all tests

--- tests/domain/fixtures/HelperDomain.php
+++ tests/domain/fixtures/HelperDomain.php
@@ -7,7 +7,7 @@ use BeholderWebClient\Eyes\Domain\Eye as OriginalEye;
 class Eye extends OriginalEye {
 
   protected function getDomainInfo(){
-    $this->domainInfo = file_get_contents(__DIR__ . '/' . $this->conf['domain']);
+    $this->domainInfo = file_get_contents(__DIR__ . '/' . $this->conf['domain'] . '.txt');
   }
 
 }
