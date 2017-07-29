1a8986b13a6323caebca70808c6c23734a7a25c3
Roger Russel <nemesis@rrussel.org>
2017-07-29 03:48:26 -0300

    fix some grammatical errors

--- doc/domain/Domain.md
+++ doc/domain/Domain.md
@@ -1,6 +1,6 @@
 # Beholder Eye - Domain
 
-This beholder eye keep an eye into Domain, if it's mounted, if it's possible to read, to write and to delete files into it.
+This beholder eye keep an eye on Domains, if it's published, if exists, or are into other status.
 
 ## Dependencies
 
@@ -14,9 +14,9 @@ require 'vendor/autoload.php';
 $conf = [
   'eyes' => [
       'MyDomain' => [ // This MyDomain is just an alias, it can be anything.
-        'type' => 'Domain', // Model wich will be used, this will be usage.
-        'domain' => 'mydomain.com.br', //Name of domain which will be keep on eye.
-        'status' => 'expectedStatus', //Status which is expected, default is "published".
+        'type' => 'Domain', // Model wich will be used.
+        'domain' => 'mydomain.com.br', //Name of the domain to keep an eye on.
+        'status' => 'expectedStatus', //Expected status. The default is "published".
         'expire' => 10, // Warning when it is n days close to expire, default: 0, RegistroBr domains only.
       ]
   ]
--- src/Eyes/Domain/DomainStatus.php
+++ src/Eyes/Domain/DomainStatus.php
@@ -6,18 +6,18 @@ use BeholderWebClient\Eyes\Status;
 
 class DomainStatus extends Status {
 
-  const REQUERIMENT_FAIL = 'REQUERIMENT FAIL: whois command not found with type -P whois';
+  const REQUERIMENT_FAIL = 'REQUERIMENT FAIL: whois command not found, it was checked with the command "type -P whois"';
   const NO_MATCH = 'Domain not found'; // error 417 Expectaion fail
 
   const UNEXPECTED_STATUS = 'The status was not the expected: ';
   const UNEXPECTED_STATUS_NUMBER = 600;
 
-  const RELEASE_PROCESS = 'Domain on realease process';
+  const RELEASE_PROCESS = 'Domain on release process';
   const RELEASE_PROCESS_NUMBER = 601;
 
-  const UNKNOW_EXPIRE_DATE = 'Unknow expire date, unfortunately this resource will work only with RegistroBR domains.';
+  const UNKNOWN_EXPIRE_DATE = 'Unknown expire date, unfortunately this resource will work only with RegistroBR domains.';
 
-  const CLOSE_TO_EXPIRE = 'Domain is this close to expire.';
+  const CLOSE_TO_EXPIRE = 'Domain is close to expire.';
   const CLOSE_TO_EXPIRE_NUMBER = 603;
 
 }
