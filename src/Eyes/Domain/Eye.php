98ec8c687d4a5e25f4e305ab33776c81840a954b
Roger Russel <roger@rrussel.org>
2017-07-31 10:59:53 -0300

    Fixes #13 Domain monitoring (#15)
    * Issue #13 started make domain monitoring, it almost finished, just finishing some tests
    
    * finished all tests
    
    * finished all tests and documentation
    
    * fix some grammatical errors
    
    * correct const UNKNOWN
    
    * fix minor grammar error

---
+++ src/Eyes/Domain/Eye.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BeholderWebClient\Eyes\Domain;
+
+use Exception;
+use BeholderWebClient\Eyes\Domain\DomainStatus as Status;
+
+Class Eye extends AbstractDomain {
+
+  public function checkRequirement(){
+
+    $result = exec('type -P whois');
+
+    if(!$result OR $result == 'whois: not found')
+      throw new Exception(Status::REQUERIMENT_FAIL, Status::INTERNAL_SERVER_ERROR_NUMBER);
+
+    if(empty($this->conf['domain']))
+      throw new Exception(Status::EXPECTATION_FAILED, Status::EXPECTATION_FAILED_NUMBER);
+  }
+
+  protected function getDomainInfo(){
+    $cmd = 'whois ';
+    $this->domainInfo = shell_exec($cmd . $this->conf['domain']);
+  }
+
+  protected function checkStatusCode() {
+
+    $m = [];
+    $r = preg_match('/^status:[\s\t]+(.*?)$/m', $this->domainInfo ,$m);
+
+    if($r === 0){
+
+      if( preg_match('/(No match for|is not registered)/', $this->domainInfo))
+        throw new Exception(Status::NO_MATCH, Status::EXPECTATION_FAILED_NUMBER);
+
+      if( preg_match('/release process/', $this->domainInfo))
+        throw new Exception(Status::RELEASE_PROCESS, Status::RELEASE_PROCESS_NUMBER);
+
+    } else {
+
+      $status = $m[1];
+
+      if(!empty($this->conf['status']) and $status !== strtolower($this->conf['status']))
+        throw new Exception(Status::UNEXPECTED_STATUS . $status, Status::UNEXPECTED_STATUS_NUMBER);
+
+      if(empty($this->conf['status']) and $status !== self::DEFAULT_STATUS )
+        throw new Exception(Status::UNEXPECTED_STATUS . $status, Status::UNEXPECTED_STATUS_NUMBER);
+
+    }
+
+  }
+
+  protected function checkExpireDate() {
+
+    $m = [];
+    $r = preg_match('/^expires:[\s\t]+([\d]+)$/m', $this->domainInfo ,$m);
+
+    if($r === 0)
+      throw new Exception(Status::UNKNOWN_EXPIRE_DATE, Status::NOT_IMPLEMENTED_NUMBER);
+
+    $date = $m[1];
+
+    if ( strtotime($date . ' -' . $this->conf['expire']. ' days') < $this->now )
+      throw new Exception(Status::CLOSE_TO_EXPIRE, Status::CLOSE_TO_EXPIRE_NUMBER);
+
+  }
+}
---
+++ src/Eyes/Domain/iDomain.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BeholderWebClient\Eyes\Domain;
+
+interface iDomain {
+
+
+}
