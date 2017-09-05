19668e19a9540423ce8f1aac9f28c205cf3fd848
Roger Russel <roger.neris@locaweb.com.br>
2017-09-05 14:51:39 -0300

    add c3 on acceptance tests

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -2,3 +2,8 @@
 
 define('__ROOT__', '/var/www');
 require __ROOT__ . '/vendor/autoload.php';
+
+if ( is_file(__DIR__ . '/../c3.php') ){
+  require __DIR__ . '/../c3.php';
+  define('MY_APP_STARTED', true);
+}
